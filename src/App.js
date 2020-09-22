22ec4fb9b192c69a968005b7673096d303c249c3
JAI-S-PAWAR <jaipawar113@gmail.com>
2020-09-22 13:33:14 +0530

    Lifted child component state, onClick() event handling, card display

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './Components/MenuComponent'
 import logo from './logo.svg';
 import './App.css';
+import {DISHES} from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">My Restaurant</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      dishes:DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">My Restaurant</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
+  
 }
+  
+
 
 export default App;
